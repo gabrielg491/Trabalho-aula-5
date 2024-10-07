2e5902792843605b9e9d271ce277b4f89e3b64ff
Diogo Vieira <diogovieira708@gmail.com>
2024-10-07 18:47:30 -0300

    Add files via upload

---
+++ Exercicio 1.py
@@ -0,0 +1,12 @@
+numero = int(input("digite um numero de 1 a 3:"))
+
+if numero == 1:
+    print ("Parabens sua Classe é BÁRBARO!")
+elif numero ==2:
+    print("Parabens sua Classe é MAGO")
+elif numero == 3:
+    print("paranems sua classe ARQUEIRO ")
+
+else:
+    print("os numeros que digitou não corresponde ao que pedi, tente novamente!")
+
